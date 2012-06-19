db4b16028640ba49502f48628ecfe804cb29b0bf
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-06-19 17:17:44 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Talk4meStaging::Application.config.secret_token = '8bdc53b7c8c096353b2fc56769f24b356835571e9fe59a8fe4148b2795bc5f5613e161ba5e3aa5e543271af9d29f28658f88b9debc38b0605c65d78cb5c0f26c'
